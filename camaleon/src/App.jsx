24554ee2f4309f96bbfabfa80f2bdd699a120261
crefazvinicius <crefazvinicius@gmail.com>
2022-12-27 15:21:02 -0300

    v 0.1

---
+++ camaleon/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9cWEzTwmO8BTViUiEioTCRMUzuStAH4w",
+  authDomain: "auth-1258a.firebaseapp.com",
+  projectId: "auth-1258a",
+  storageBucket: "auth-1258a.appspot.com",
+  messagingSenderId: "839698163153",
+  appId: "1:839698163153:web:19532e7df3ef05b281acf0"
+};
+
+// Get a reference to the database
+var database = firebase.database();
+
+// Read data from the database
+database.ref("/posts").on("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+// Write data to the database
+database.ref("/posts").push({
+  title: "Hello, World!",
+  content: "This is a sample post."
+});
+
+const app = initializeApp(firebaseConfig);
+
+
+export default App
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
