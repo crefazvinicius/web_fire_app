79c097d32fb03eaeb007b968ec7909313b3f95a5
crefazvinicius <crefazvinicius@gmail.com>
2022-12-27 18:14:36 -0300

    v

--- camaleon/src/App.jsx
+++ camaleon/src/App.jsx
@@ -11,6 +11,22 @@ const firebaseConfig = {
   messagingSenderId: "839698163153",
   appId: "1:839698163153:web:19532e7df3ef05b281acf0"
 };
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+  
 
 // Get a reference to the database
 var database = firebase.database();
